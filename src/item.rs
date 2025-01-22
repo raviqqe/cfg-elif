bd2de077d03f77d1eebe6e8a1cf1cacfde5c11af
Yota Toyama <raviqqe@gmail.com>
2025-01-23 01:00:03 +0900

    Not operator

--- src/item.rs
+++ src/item.rs
@@ -49,6 +49,8 @@ macro_rules! item_feature {
 ///     type Foo = i32;
 /// } else if ((feature == "baz") || (target_os == "freebsd")) {
 ///     type Foo = i64;
+/// } else if (!(panic == "unwind")) {
+///     type Foo = i128;
 /// } else {
 ///     type Foo = f64;
 /// });
@@ -87,6 +89,13 @@ macro_rules! item_cfg {
             $then1
         } else {
             $crate::item_cfg!($(if $condition { $then2 } else)* { $else });
+        });
+    };
+    (if (!$condition1:tt) { $then1:item } else $(if $condition2:tt { $then2:item } else)* { $else:item }) => {
+        $crate::item_cfg!(if $condition1 {
+            $crate::item_cfg!($(if $condition2 { $then2 } else)* { $else });
+        } else {
+            $then1
         });
     };
     ({ $else:item }) => {
